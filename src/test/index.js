42e5a505c3b8ac18ff4d27639ee0387c536f0b70
dchima <dcnnad@gmail.com>
2019-09-27 06:51:56 +0100

    ch: add coveralls with badge

---
+++ src/test/index.js
@@ -0,0 +1,9 @@
+import chai from 'chai';
+
+const { expect } = chai;
+
+describe('Initial test', () => {
+  it('should be equal to kampe sports', () => {
+    expect('kampe sports').to.equal('kampe sports');
+  });
+});
