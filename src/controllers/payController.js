6d507cfaf30009e527f3d6a8fedba80ee375af62
dchima <dcnnad@gmail.com>
2019-12-26 10:36:20 +0100

    fixed npm start

--- src/controllers/payController.js
+++ src/controllers/payController.js
@@ -34,32 +34,32 @@ export default class PayController {
        * PLEASE UNCOMMENT ALL LINES BELOW BEFORE PRODUCTION
        * OR WHEN TESTING PAYSTACK API FOR YOURSELF
        */
-      // const mealsInCart = await getMealsInCart({ userId: req.tokenData.id });
-      // if (!mealsInCart.length) return errorResponse(res, { code: 404, message: 'There are no meals in your cart' });
-      // const meals = mealsInCart.map(({
-      //   id, userId, createdAt, updatedAt, ...items
-      // }) => items);
-      // const mealIds = meals.map((meal) => meal.mealId);
-      // const mealsInDatabase = await findMultipleMeals({ id: mealIds });
-      // const price = calculateOrderPrice(meals, mealsInDatabase);
-      // const metadata = {
-      //   userId: req.tokenData.id, meals, address, price,
-      // };
-      // const payload = {
-      //   email: req.tokenData.email,
-      //   amount: price * 100,
-      //   metadata,
-      // };
+      const mealsInCart = await getMealsInCart({ userId: req.tokenData.id });
+      if (!mealsInCart.length) return errorResponse(res, { code: 404, message: 'There are no meals in your cart' });
+      const meals = mealsInCart.map(({
+        id, userId, createdAt, updatedAt, ...items
+      }) => items);
+      const mealIds = meals.map((meal) => meal.mealId);
+      const mealsInDatabase = await findMultipleMeals({ id: mealIds });
+      const price = calculateOrderPrice(meals, mealsInDatabase);
+      const metadata = {
+        userId: req.tokenData.id, meals, address, price,
+      };
+      const payload = {
+        email: req.tokenData.email,
+        amount: price * 100,
+        metadata,
+      };
 
-      // const paystack = await Payments.viaPaystack(payload);
-      // if (!paystack.status) errorResponse(res, { code: 400, message: paystack.message });
-      // const url = paystack.data.authorization_url;
-      // successResponse(res, { message: `success, redirect to ${url}`, url });
+      const paystack = await Payments.viaPaystack(payload);
+      if (!paystack.status) errorResponse(res, { code: 400, message: paystack.message });
+      const url = paystack.data.authorization_url;
+      successResponse(res, { message: `success, redirect to ${url}`, url });
       /**
        * PLEASE DELETE SINGLE LINE BELOW BEFORE PRODUCTION
        * OR COMMENT WHEN TESTING PAYSTACK API FOR YOURSELF
        */
-      successResponse(res, { message: 'success, redirect to https://greenpot-api.herokuapp.com/v1.0/api/docs' });
+      // successResponse(res, { message: 'success, redirect to https://greenpot-api.herokuapp.com/v1.0/api/docs' });
     } catch (error) {
       if (error === 'Please enter a strring') return errorResponse(res, { code: 400, message: error });
       errorResponse(res, {});
